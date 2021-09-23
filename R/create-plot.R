134e8b13a7d71f9a3eae3d9dd942d7a7f36cf371
Shan Wilkinson <shanwilkinson2@yahoo.co.uk>
2021-09-23 11:41:05 +0100

    new chart types

--- R/create-plot.R
+++ R/create-plot.R
@@ -9,18 +9,37 @@ df = read_csv("data/weatherAUS.csv")
 # Create a scatter plot of Humidity9am and Temp9am
 # a location
 
-city = "SalmonGums"
-
-scatter_plot = 
-df %>%
-  filter(Location == city) %>%
-  ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(color = "red", size = 0.5, alpha = 0.2) +
-  ggtitle(city) +
-  labs(y = "Humidity 9am", x = "Temperature 9am") +
-  geom_smooth(method = "lm", color = "black", se = FALSE) + 
-  theme_classic()
+  city = "SalmonGums"
   
+  scatter_plot = 
+  df %>%
+    filter(Location == city) %>%
+    ggplot(aes(x = Temp9am, y = Humidity9am)) + 
+    geom_point(color = "red", size = 0.5, alpha = 0.2) +
+    ggtitle(city) +
+    labs(y = "Humidity 9am", x = "Temperature 9am") +
+    geom_smooth(method = "lm", color = "black", se = FALSE, linetype = "dashed", size = 0.5) + 
+    theme_classic()
+    
+  
+  # Save the plot in graphics/
+  ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
 
-# Save the plot in graphics/
-ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
+# bar plot
+  bar_chart =
+    df %>%
+    group_by(Location) %>%
+    summarise(average_rainfall = mean(Rainfall, na.rm = TRUE)) %>%
+
+    ggplot(aes(y = Location, x = average_rainfall)) +
+    geom_col(fill = "green")
+  ggsave(filename = "graphics/barchart-avg-rainfall.png", plot = bar_chart)
+  
+  
+  time_series =
+    df %>%
+    filter(Location == city) %>%
+    ggplot(aes(x = Date, y = Temp9am)) +
+    geom_line()
+  ggsave(filename = "graphics/timeseries-rainfall.png",
+         plot = time_series)
