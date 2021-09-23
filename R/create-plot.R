4eaf609fb0a81738300da3ec256226073864d7ff
Shan Wilkinson <shanwilkinson2@yahoo.co.uk>
2021-09-23 09:51:04 +0100

    graph changed agani

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,11 +15,11 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(color = "red") +
+  geom_point(color = "red", size = 0.5, alpha = 0.2) +
   ggtitle(city) +
   labs(y = "Humidity 9am", x = "Temperature 9am") +
   geom_smooth(method = "lm", color = "black", se = FALSE) + 
-  theme_grey()
+  theme_classic()
   
 
 # Save the plot in graphics/
